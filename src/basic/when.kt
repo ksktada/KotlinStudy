070c53a77be1bf77ce7b0b51359f56ddd47c9a7c
Keisuke TADA <ksk.tada@gmail.com>
2020-01-02 23:16:05 +0900

    Create when.kt

---
+++ src/basic/when.kt
@@ -0,0 +1,14 @@
+import java.util.*
+ 
+fun main(args: Array<String>) {
+    println(describe("Hello"))
+}
+
+fun describe(obj: Any): String =
+    when (obj) {
+        1          -> "One"
+        "Hello"    -> "Greeting"
+        is Long    -> "Long"
+        !is String -> "Not a string"
+        else       -> "Unknown"
+    }
